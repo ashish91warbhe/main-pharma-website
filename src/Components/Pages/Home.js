cd37c0b658d9f6fc95b86a564fb924f2871d0f2e
Ashish Warbhe <ashish91warbhe@gmail.com>
2022-12-11 18:35:08 +0530

    first commit

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
 //import React from 'react'
 //import React from 'react'
-
-
+//import React from 'react'
+//import React from 'react'
 const wh = {
     width:"100%",
     height:"100%"
